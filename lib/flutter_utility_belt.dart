fd8b85e8743e16c73515e5e410c2932bb3c40015
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-07-31 23:57:48 -0300

    feat: adds labeled icon widget

---
+++ lib/flutter_utility_belt.dart
@@ -0,0 +1,4 @@
+/// Main place to utitlities
+library flutter_utility_belt;
+
+export './widgets/labeled_icon.dart';
---
+++ lib/widgets/labeled_icon.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/widgets.dart';
+
+/// The [LabeledIcon] class is a base class that extends [StatelessWidget].
+/// It defines properties for an [icon], a [gap], a [label], and an [axis].
+/// The constructor [LabeledIcon] initializes these properties and
+/// allows for the [axis] to default to [Axis.horizontal] if not specified.
+base class LabeledIcon extends StatelessWidget {
+  final Widget icon;
+  final double gap;
+  final Widget label;
+  final Axis axis;
+
+  const LabeledIcon({
+    super.key,
+    required this.icon,
+    required this.gap,
+    required this.label,
+    this.axis = Axis.horizontal,
+  });
+
+  factory LabeledIcon.vertical({
+    required Widget icon,
+    required Widget label,
+    required double gap,
+  }) =>
+      VerticalLabeledIcon(icon: icon, label: label, gap: gap);
+
+  factory LabeledIcon.horizontal({
+    required Widget icon,
+    required Widget label,
+    required double gap,
+  }) =>
+      HorizontalLabeledIcon(icon: icon, label: label, gap: gap);
+
+  @override
+  Widget build(BuildContext context) => switch (axis) {
+        Axis.horizontal =>
+          HorizontalLabeledIcon(icon: icon, label: label, gap: gap),
+        Axis.vertical =>
+          VerticalLabeledIcon(icon: icon, label: label, gap: gap),
+      };
+}
+
+/// It is responsible for displaying an [icon] and a [label] vertically with a
+/// specified [gap] between them.
+final class VerticalLabeledIcon extends LabeledIcon {
+  const VerticalLabeledIcon({
+    super.key,
+    required super.icon,
+    required super.label,
+    required super.gap,
+  }) : super(axis: Axis.vertical);
+
+  @override
+  Widget build(BuildContext context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          icon,
+          SizedBox(
+            height: gap,
+          ),
+          label,
+        ],
+      );
+}
+
+/// The [HorizontalLabeledIcon] class is responsible for displaying an [icon]
+/// and a [label] horizontally with a specified [gap] between them.
+final class HorizontalLabeledIcon extends LabeledIcon {
+  const HorizontalLabeledIcon({
+    super.key,
+    required super.icon,
+    required super.label,
+    required super.gap,
+  }) : super(axis: Axis.horizontal);
+
+  @override
+  Widget build(BuildContext context) => Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          icon,
+          SizedBox(
+            width: gap,
+          ),
+          label,
+        ],
+      );
+}
